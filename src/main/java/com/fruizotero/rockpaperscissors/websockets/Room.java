337a6826454cf9e08d9cc81e761e6fdc314dce46
fruizotero <frank.git96@gmail.com>
2023-12-28 01:42:29 +0100

    (Room) Métodos para unirse, salir y enviar mensajes

--- src/main/java/com/fruizotero/rockpaperscissors/websockets/Room.java
+++ src/main/java/com/fruizotero/rockpaperscissors/websockets/Room.java
@@ -3,18 +3,37 @@ package com.fruizotero.rockpaperscissors.websockets;
 import jakarta.websocket.Session;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class Room {
 
     private String nameRoom;
     private HashMap<String, Session> sessions = new HashMap<>();
 
-    public Room (String nameRoom){
-        this.nameRoom=nameRoom;
+    public Room(String nameRoom) {
+        this.nameRoom = nameRoom;
     }
 
+    public void joinToRoom(Session session) {
+        sessions.put(session.getId(), session);
+    }
 
+    public void exitToRoom(Session session) {
+        sessions.remove(session.getId());
+    }
 
+    public void sendMessage(String idWinner) {
+//TODO:: mensaje para los participantes
+        for (Map.Entry<String, Session> entry : sessions.entrySet()) {
+            String id = entry.getKey();
+            Session session = entry.getValue();
+            if (id.equalsIgnoreCase(idWinner)) {
+                session.getAsyncRemote().sendText("Has ganado");
+            } else{
+                session.getAsyncRemote().sendText("Has perdido");
+            }
+        }
+    }
 
 
 }

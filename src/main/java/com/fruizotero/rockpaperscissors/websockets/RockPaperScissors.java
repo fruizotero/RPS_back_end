0b458d86a707c31367664e0664fe66695e919e38
fruizotero <frank.git96@gmail.com>
2024-01-07 23:19:23 +0100

    (RockPaperScissors) Mejora de la lógica. Sólo funcional para 2 usuarios

--- src/main/java/com/fruizotero/rockpaperscissors/websockets/RockPaperScissors.java
+++ src/main/java/com/fruizotero/rockpaperscissors/websockets/RockPaperScissors.java
@@ -1,44 +1,30 @@
 package com.fruizotero.rockpaperscissors.websockets;
 
-import jakarta.websocket.Session;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
-
 public class RockPaperScissors {
 
 
-    public static Map<String, Peer> getWinner(Peer p1, Peer p2) {
+    public static void getWinner(Peer p1, Peer p2) {
 
-        String idPeer1 = p1.getIdPeer();
-        String idPeer2 = p2.getIdPeer();
-        Map<String, String> listResults = new HashMap<>();
-        listResults.put("R|S", idPeer1);
-        listResults.put("P|R", idPeer1);
-        listResults.put("S|P", idPeer1);
-        Map<String, Peer> results = new HashMap<>();
-
+        String stringResults = "R|S P|R S|P";
         String choicePeer1 = p1.getChoice();
         String choicePeer2 = p2.getChoice();
+        String choices = "";
 
         if (choicePeer1.equalsIgnoreCase(choicePeer2)) {
-            results.put("draw", null);
-            return results;
+            p1.setDraw(true);
+            p2.setDraw(true);
+            return;
         }
 
-        String idWinner = listResults.getOrDefault(String.format("%s|%s", choicePeer1, choicePeer2), null);
-
-        if (idWinner != null) {
-            results.put("winner", p1);
-            results.put("loser", p2);
+        choices = String.format("%s|%s", p1.getChoice(), p2.getChoice());
+        if (stringResults.contains(choices)) {
+            p1.setWinner(true);
+            p2.setWinner(false);
         } else {
-            results.put("winner", p2);
-            results.put("loser", p1);
+            p1.setWinner(false);
+            p2.setWinner(true);
         }
 
-
-        return results;
     }
 
 }
